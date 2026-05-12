6fb4bed00992442fb4f166ad98b7fa505428aed8
Astin-Salivio <justin.salivio2580@gmail.com>
2026-05-12 10:12:15 +0800

    Seperation

---
+++ script.js
@@ -0,0 +1,81 @@
+/* ================= LOGIN ================= */
+
+function login(){
+
+  const username =
+  document.getElementById("username");
+
+  const password =
+  document.getElementById("password");
+
+  if(username && password){
+
+    if(username.value === "" || password.value === ""){
+
+      alert("Please fill in all fields.");
+
+    }
+
+    else{
+
+      alert("Login Successful!");
+
+      window.location.href = "main.html";
+
+    }
+
+  }
+
+}
+
+/* ================= SETTINGS ================= */
+
+const settingsPanel =
+document.getElementById("settingsPanel");
+
+function toggleSettings(){
+
+  if(settingsPanel){
+
+    settingsPanel.classList.toggle("active");
+
+  }
+
+}
+
+/* ================= DARK MODE ================= */
+
+function toggleDarkMode(){
+  document.body.classList.toggle("dark-mode");
+}
+
+/* ================= LOGOUT ================= */
+
+function logout(){
+  window.location.href = "login.html";
+}
+
+/* ================= NOTIFICATIONS ================= */
+
+const notificationPanel =
+document.getElementById("notificationPanel");
+
+function openNotifications(){
+
+  if(notificationPanel){
+
+    notificationPanel.classList.add("active");
+
+  }
+
+}
+
+function closeNotifications(){
+
+  if(notificationPanel){
+
+    notificationPanel.classList.remove("active");
+
+  }
+
+}
